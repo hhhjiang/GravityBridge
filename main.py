656983fd444ba90a84e9860eb4bb57a6c624d78f
hhhjiang <37356598+hhhjiang@users.noreply.github.com>
2026-01-05 16:12:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import sys
+import os
 from PySide6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
-from PySide6.QtGui import QAction, QIcon
+from PySide6.QtGui import QIcon, QAction
+
+def resource_path(filename):
+    # PyInstaller 兼容路径
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, filename)
+    return os.path.join(os.path.abspath("."), filename)
 
 def main():
     app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
 
-    tray = QSystemTrayIcon()
-    tray.setIcon(QIcon())  # 没 icon 也能跑
+    icon_path = resource_path("icon.ico")
+    tray = QSystemTrayIcon(QIcon(icon_path))
 
     menu = QMenu()
 
